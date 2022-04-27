10c4804f16c114d8d12dc78dd2acbddcd94f207f
Ali Amsal Surani <ali.surani7@gmail.com>
2022-04-27 14:12:38 +0500

    Time: 33 ms (87.31%), Space: 13.8 MB (75.29%) - LeetHub

---
+++ 67-add-binary/67-add-binary.py
@@ -0,0 +1,18 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        n1 = 0
+        n2 = 0
+        for i in range(len(a)-1,-1,-1):
+            #print(a[i])
+            if a[i] == "1":
+                n1 += 2**(len(a)-i-1)
+                #print("hi")
+        print(n1)
+        for i in range(len(b)-1,-1,-1):
+            #print(a[i])
+            if b[i] == "1":
+                n2 += 2**(len(b)-i-1)
+                #print("hi")
+        print(n2)
+        ans = n1+n2
+        return bin(ans)[2:]
